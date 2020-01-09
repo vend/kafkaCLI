52e25f696d870015f5bb3a4c630c9b9716ba0d70
Gustavo Soares Souza <gustavo@vendhq.com>
2020-01-09 14:25:27 +1300

    partitioner initial commit

---
+++ cmd/partitioner.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+	"github.com/spf13/cobra"
+)
+
+// partitionerCmd finds partition associated to a given key
+var partitionerCmd = &cobra.Command{
+	Use:   "partitioner",
+	Short: "Finds partition associated to a given key",
+	Long: `createTopic can create one or more topics. Example:
+
+kafkaCLI partitioner --topic blackhole --partition-count 16 --message test --key-id 00005a30-9766-11e3-a0f5-b8ca3a64f8f4
+`,
+	Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		keyIdString := sarama.StringEncoder(keyID)
+	
+		part := sarama.NewHashPartitioner(topic)
+		msg := &sarama.ProducerMessage{Topic: topic, Key: keyIdString, Value: sarama.StringEncoder(message)}
+	
+		partition, err := part.Partition(msg, int32(partitions))
+		
+		if err != nil {
+			panic(err)
+		}
+	
+		fmt.Printf("partition: %v key-id: %v\n", partition, keyIdString)
+
+	},
+}
+
+var topic string
+var partitionCount int16
+var message string
+var keyID string
+
+func init() {
+	rootCmd.AddCommand(partitionerCmd)
+
+	partitionerCmd.Flags().StringVarP(&topic, "topic", "p", "blackhole", "topic name")
+	partitionerCmd.Flags().Int16VarP(&partitionCount, "particiout-count", "c", 16, "number of partitions for the topic")
+	partitionerCmd.Flags().StringVarP(&message, "message", "m", "test", "message text")
+	partitionerCmd.Flags().StringVarP(&keyID, "key-id", "k", "", "key id used by the message")
+}
